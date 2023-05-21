5874011f7f131826d67747384be5027110e08be1
Hayden D’Souza <haydenfd@Haydens-MBP.lan>
2023-05-21 00:25:22 -0700

    Init pt 2

---
+++ server/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.live.nba.endpoints import scoreboard
+from fastapi.encoders import jsonable_encoder
+
+
+
+app = FastAPI()
+
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/get-today-games")
+async def get_today_games():
+    board = scoreboard.ScoreBoard()
+    games = board.games.get_dict()
+    return jsonable_encoder(games)
+
+
+@app.get("/")
+def read_root():
+    career = playercareerstats.PlayerCareerStats(player_id='203999') 
+    print(career.get_data_frames()[0])
+
+
+
