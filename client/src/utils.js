35483b4f7e9a2272d72d79685b13077437125b33
Hayden D’Souza <haydenfd@Haydens-MBP.lan>
2023-05-21 02:57:53 -0700

    Added home page, game card containers

---
+++ client/src/utils.js
@@ -0,0 +1,51 @@
+const SERVER_URL = 'http://127.0.0.1:8000'
+
+const ENDPOINTS = 
+{
+    getTodayGames: '/get-today-games'
+}
+
+const MONTHS = [
+    "buffer",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
+
+
+const GAMEDAYFORMATTER = (gameDay) => 
+{
+    return `${MONTHS[parseInt(gameDay.month)]} ${gameDay.date}, ${gameDay.year} games`
+
+}
+
+const MAPTRICODETOICON = (tricodeString) => 
+{
+    switch (tricodeString) 
+    {
+        case "BOS":
+            return 0
+        case "DEN":
+            return 1
+        case "LAL":
+            return 2
+        case "MIA":
+            return 3
+    }
+}
+
+export {
+    SERVER_URL,
+    GAMEDAYFORMATTER,
+    ENDPOINTS,
+    MAPTRICODETOICON,
+}
