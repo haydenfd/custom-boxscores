d5eabe7ac7033ebdf63e70434b539db17583cf72
Hayden D’Souza <haydenfd@wifi-131-179-55-236.host.ucla.edu>
2023-05-22 17:13:47 -0700

    Added pref form elements

--- client/src/Pages/PreferencesForm.jsx
+++ client/src/Pages/PreferencesForm.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import './PreferencesForm.css'
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+import Box from '@mui/material/Box';
 
 const theme = createTheme({
     typography: {
-      fontSize: 16,
+      fontSize: 20,
+      fontWeight: 700,
       fontFamily: [
         'Avenir',
         'sans-serif'
@@ -19,40 +19,115 @@ const theme = createTheme({
   });
   
 
-const PreferencesForm = () => {
+const PreferencesForm = () => 
+{
 
+    const boxSx={
+        display: 'flex',
+        flexDirection: 'column',
+    }
 
     return (
-    <>
-        <h1>
-            Your Preferences
-        </h1>
-        {/* <div className="form-wrapper"> */}
-            <ThemeProvider theme={theme}>
+        <>
+        <ThemeProvider theme={theme}>
+        <h1>Your Preferences</h1>
+        <Box sx={boxSx}>     
             <FormGroup>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="MINS" sx={{ '& .MuiSvgIcon-root': { fontSize: 34} }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="PTS" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="AST" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="BLK" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="STL" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="3PT" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="FT" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="FT %" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="FG" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="FG %" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="OREB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="DREB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="REB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="+/-" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="PF" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
-                <FormControlLabel control={<Checkbox color="secondary"/>} label="TOV" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+                <FormControlLabel
+                    control={<Checkbox color="secondary" name="mins"/>} 
+                    label="Minutes" 
+                    sx={{ '& .MuiSvgIcon-root': { fontSize: 28} }}
+                    />
+                <FormControlLabel
+                    control={<Checkbox color="secondary" name="pts"/>} 
+                    label="Points" 
+                    sx={{ '& .MuiSvgIcon-root': { fontSize: 28} }}
+                    />     
+                <FormControlLabel
+                    control={<Checkbox color="secondary" name="ast"/>} 
+                    label="Assists" 
+                    sx={{ '& .MuiSvgIcon-root': { fontSize: 28} }}
+                    />  
+                <FormControlLabel
+                    control={<Checkbox color="secondary" name="pts"/>} 
+                    label="Pts" 
+                    sx={{ '& .MuiSvgIcon-root': { fontSize: 28} }}
+                    />                                                         
             </FormGroup>
-            </ThemeProvider>
-        {/* </div> */}
-        
-    </>
+        </Box>
+        </ThemeProvider>
+        </>
     )
-
 }
 
-export default PreferencesForm;
+export default PreferencesForm
+
+// const PreferencesForm = () => {
+
+//     const handleChange = (e) => 
+//     {
+//         const name = e.target.name
+//         if (e.target.checked)
+//         {
+        
+//         }
+
+//         setList({
+//             ...list,
+//             [e.target.name]: e.target.checked,
+//         })
+//     }
+
+//     const [list, setList] = useState({
+//         "mins": false,
+//         "pts": false,
+//         "ast": false,
+//     })
+
+//     return (
+//     <>
+//         <h1>
+//             Your Preferences
+//         </h1>
+//             <ThemeProvider theme={theme}>
+//             <FormGroup>
+                // <FormControlLabel
+                // control={<Checkbox color="secondary" onChange={handleChange} name="mins"
+                // />} 
+                // label="MINS" 
+                // sx={{ '& .MuiSvgIcon-root': { fontSize: 34} }}
+                // />
+//                 <FormControlLabel
+//                  control={<Checkbox color="secondary" name="pts" onChange={handleChange}
+//                  />} 
+//                  label="PTS" 
+//                  sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}
+//                  />
+//                 <FormControlLabel 
+//                 control={<Checkbox color="secondary" onChange={handleChange} name="ast"/>} 
+//                 label="AST" 
+//                 sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}
+//                 />
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="BLK" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="STL" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="3PT" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="FT" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="FT %" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="FG" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="FG %" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="OREB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="DREB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="REB" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="+/-" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="PF" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//                 <FormControlLabel control={<Checkbox color="secondary"/>} label="TOV" sx={{ '& .MuiSvgIcon-root': { fontSize: 34 } }}/>
+//             </FormGroup>
+//             </ThemeProvider>
+//         </div>
+        
+//     </>
+//     )
+
+// }
+
+// export default PreferencesForm;
